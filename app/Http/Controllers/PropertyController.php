737325340c101463e4b0d100123b63d99941cdd1
Razib Rahman <razibeee2014@gmail.com>
2023-08-18 01:12:38 +0600

    jhk

--- app/Http/Controllers/PropertyController.php
+++ app/Http/Controllers/PropertyController.php
@@ -30,9 +30,9 @@ class PropertyController extends Controller
         return Inertia::render(
             "Dashboard/Property/Create",
             [
-                'locations'=>$locations,
-                'types'=>$types,
-                'features'=>$features,
+                'locations' => $locations,
+                'types' => $types,
+                'features' => $features,
             ]
         );
     }
@@ -41,7 +41,7 @@ class PropertyController extends Controller
     {
         $properties = Property::orderby('id', 'desc')->get();
         return Inertia::render("Dashboard/Property/Index", [
-            'properties' =>$properties,
+            'properties' => $properties,
         ]);
     }
 
@@ -60,60 +60,60 @@ class PropertyController extends Controller
             'image_gallery' => 'required',
 
         ]);
-        $photo = (isset($request['image']) && $request['image']!= "") ? $request['image'] : "";
-        if ($photo!="") {
+        $photo = (isset($request['image']) && $request['image'] != "") ? $request['image'] : "";
+        if ($photo != "") {
             $ext                    = $photo->getClientOriginalExtension();
             $photoFullName          = time().$photo->getClientOriginalName();
             $uploadPath             = 'images/';
             $success                = $photo->move($uploadPath, $photoFullName);
         }
         $property = Property::create([
-            'title'             =>$request->title,
-            'slug'              =>Str::slug($request->title) ,
-            'location_id'       =>1,
-            'price'             =>$request->price,
-            'type_id'           =>$request->type_id,
-            'bed'               =>$request->bed ? $request->bed : "",
-            'bath'              =>$request->bath ? $request->bath : "",
-            'grage'             =>$request->grage ? $request->grage : "",
-            'city'              =>$request->city,
-            'zip_code'          =>$request->zip_code,
-            'country'           =>"Bangladesh",
+            'title'             => $request->title,
+            'slug'              => Str::slug($request->title) ,
+            'location_id'       => 1,
+            'price'             => $request->price,
+            'type_id'           => $request->type_id,
+            'bed'               => $request->bed ? $request->bed : "",
+            'bath'              => $request->bath ? $request->bath : "",
+            'grage'             => $request->grage ? $request->grage : "",
+            'city'              => $request->city,
+            'zip_code'          => $request->zip_code,
+            'country'           => "Bangladesh",
             // 'user_id'             =>$request->user_id,
-            'sqf'               =>$request->sqf ? $request->sqf : "",
-            'balcony'           =>$request->balcony,
-            'floor'             =>$request->floor,
-            'unit_no'           =>$request->unit_no,
-            'unit_per_floor'    =>$request->unit_per_floor,
-            'maid_room'         =>$request->maid_room,
-            'service_charge'    =>$request->service_charge,
-            'phone'             =>$request->phone ? $request->phone : "",
-            'date'              =>$request->date,
+            'sqf'               => $request->sqf ? $request->sqf : "",
+            'balcony'           => $request->balcony,
+            'floor'             => $request->floor,
+            'unit_no'           => $request->unit_no,
+            'unit_per_floor'    => $request->unit_per_floor,
+            'maid_room'         => $request->maid_room,
+            'service_charge'    => $request->service_charge,
+            'phone'             => $request->phone ? $request->phone : "",
+            'date'              => $request->date,
             // 'description'       =>$request->description,
-            'address'           =>$request->address,
+            'address'           => $request->address,
             // 'details'           =>$request->details,
-            'map_link'          =>$request->map_link,
-            'whatsapp_number'   =>$request->whatsapp_number,
-            'created_by'        =>Auth::user()->id,
+            'map_link'          => $request->map_link,
+            'whatsapp_number'   => $request->whatsapp_number,
+            'created_by'        => Auth::user()->id,
             'rating'            => 0, //$request->rating,
             // 'share_link'        =>$request->share_link,
-            'share_count'       =>0, //$request->share_count,
-            'call_count'        =>0, //$request->call_count,
-            'visitor_count'     =>0, //$request->visitor_count,
-            'status'            =>1, //$request->status,
-            'publisher_status'  =>$request->publisher_status,
-            'recived_count'     =>0, //$request->recived_count,
+            'share_count'       => 0, //$request->share_count,
+            'call_count'        => 0, //$request->call_count,
+            'visitor_count'     => 0, //$request->visitor_count,
+            'status'            => 1, //$request->status,
+            'publisher_status'  => $request->publisher_status,
+            'recived_count'     => 0, //$request->recived_count,
           //   'image'            => '/'.$uploadPath.$photoFullName,
              'image'            => '',
-            'video_link'        =>$request->video_link,
+            'video_link'        => $request->video_link,
             'meta_title'        => '', //$request->meta_title,
-            'meta_description'  =>'', //$request->meta_description,
-            'meta_tag'          =>'', //$request->meta_tag,
-            'meta_keyward'      =>'', //$request->meta_keyward
+            'meta_description'  => '', //$request->meta_description,
+            'meta_tag'          => '', //$request->meta_tag,
+            'meta_keyward'      => '', //$request->meta_keyward
         ]);
         if($property->id) {
             $galleriesData = new PropertyImageGallery();
-            $galleriesData->property_id =$property->id;
+            $galleriesData->property_id = $property->id;
             $galleriesData->images = '/'.$uploadPath.$photoFullName;
             $galleriesData->save();
 
@@ -141,15 +141,15 @@ class PropertyController extends Controller
         }
 
         foreach ($request->image_gallery as $gallery) {
-            $photo = (isset($gallery) && $gallery!= "") ? $gallery : "";
-            if ($photo!="") {
+            $photo = (isset($gallery) && $gallery != "") ? $gallery : "";
+            if ($photo != "") {
                 $ext                    = $photo->getClientOriginalExtension();
                 $gallerFullName          = time().$photo->getClientOriginalName();
                 $uploadPath             = 'images/';
                 $success                = $photo->move($uploadPath, $gallerFullName);
             }
             $galleriesData = new PropertyImageGallery();
-            $galleriesData->property_id =$property->id;
+            $galleriesData->property_id = $property->id;
             $galleriesData->images = '/'.$uploadPath.$gallerFullName;
             $galleriesData->save();
         }
@@ -168,10 +168,10 @@ class PropertyController extends Controller
 
         // return $property;
         return Inertia::render("Dashboard/Property/Edit", [
-            'properties' =>$property,
-            'locations'=>$locations,
-                'types'=>$types,
-                'features'=>$features,
+            'properties' => $property,
+            'locations' => $locations,
+                'types' => $types,
+                'features' => $features,
         ]);
 
 
@@ -193,59 +193,58 @@ class PropertyController extends Controller
 
 
         $property->update([
-            'title'             =>$request->title,
-            'slug'              =>Str::slug($request->title) ,
-            'location_id'       =>$request->location_id,
-            'price'             =>$request->price,
-            'type_id'           =>$request->type_id,
-            'bed'               =>$request->bed,
-            'bath'              =>$request->bath,
-            'grage'             =>$request->grage,
-            'city'             =>$request->city,
-            'zip_code'             =>$request->zip_code,
-            'country'             =>"Bangladesh",
-            // 'user_id'            use Illuminate\Support\Facades\Validator;
- =>$request->user_id,
-            'sqf'               =>$request->sqf,
-            'balcony'               =>$request->balcony,
-            'floor'               =>$request->floor,
-            'unit_no'               =>$request->unit_no,
-            'unit_per_floor'               =>$request->unit_per_floor,
-            'maid_room'               =>$request->maid_room,
-            'service_charge'               =>$request->service_charge,
-            'phone'             =>$request->phone,
-            'date'              =>$request->date,
+            'title'             => $request->title,
+            'slug'              => Str::slug($request->title) ,
+            'location_id'       => $request->location_id,
+            'price'             => $request->price,
+            'type_id'           => $request->type_id,
+            'bed'               => $request->bed,
+            'bath'              => $request->bath,
+            'grage'             => $request->grage,
+            'city'             => $request->city,
+            'zip_code'             => $request->zip_code,
+            'country'             => "Bangladesh",
+            // 'user_id'            =>$request->user_id,
+            'sqf'               => $request->sqf,
+            'balcony'               => $request->balcony,
+            'floor'               => $request->floor,
+            'unit_no'               => $request->unit_no,
+            'unit_per_floor'               => $request->unit_per_floor,
+            'maid_room'               => $request->maid_room,
+            'service_charge'               => $request->service_charge,
+            'phone'             => $request->phone,
+            'date'              => $request->date,
             // 'description'       =>$request->description,
-            'address'           =>$request->address,
+            'address'           => $request->address,
             // 'details'           =>$request->details,
-            'map_link'          =>$request->map_link,
-            'whatsapp_number'   =>$request->whatsapp_number,
-            'created_by'        =>Auth::user()->id,
+            'map_link'          => $request->map_link,
+            'whatsapp_number'   => $request->whatsapp_number,
+            'created_by'        => Auth::user()->id,
             'rating'            => 0, //$request->rating,
             // 'share_link'        =>$request->share_link,
-            'share_count'       =>0, //$request->share_count,
-            'call_count'        =>0, //$request->call_count,
-            'visitor_count'     =>0, //$request->visitor_count,
-            'status'            =>1, //$request->status,
-            'publisher_status'  =>$request->publisher_status,
-            'recived_count'     =>0, //$request->recived_count,
-            'video_link'        =>$request->video_link,
+            'share_count'       => 0, //$request->share_count,
+            'call_count'        => 0, //$request->call_count,
+            'visitor_count'     => 0, //$request->visitor_count,
+            'status'            => 1, //$request->status,
+            'publisher_status'  => $request->publisher_status,
+            'recived_count'     => 0, //$request->recived_count,
+            'video_link'        => $request->video_link,
             'meta_title'        => '', //$request->meta_title,
-            'meta_description'  =>'', //$request->meta_description,
-            'meta_tag'          =>'', //$request->meta_tag,
-            'meta_keyward'      =>'', //$request->meta_keyward
+            'meta_description'  => '', //$request->meta_description,
+            'meta_tag'          => '', //$request->meta_tag,
+            'meta_keyward'      => '', //$request->meta_keyward
         ]);
         if($request['image'] != null || $request['image'] != '') {
 
-            $photo = (isset($request['image']) && $request['image']!= "") ? $request['image'] : "";
-            if ($photo!="") {
+            $photo = (isset($request['image']) && $request['image'] != "") ? $request['image'] : "";
+            if ($photo != "") {
                 $ext                    = $photo->getClientOriginalExtension();
                 $photoFullName          = time().$photo->getClientOriginalName();
                 $uploadPath             = 'images/';
                 $success                = $photo->move($uploadPath, $photoFullName);
             }
             $galleriesData = new PropertyImageGallery();
-            $galleriesData->property_id =$request->id;
+            $galleriesData->property_id = $request->id;
             $property->image = '/'.$uploadPath.$photoFullName;
             $property->save();
         }
@@ -261,8 +260,8 @@ class PropertyController extends Controller
         if(is_array($request->image_gallery)) {
             $galleriDelete = PropertyImageGallery::where('property_id', $request->id)->delete();
             foreach ($request->image_gallery as $gallery) {
-                $photo = (isset($gallery) && $gallery!= "") ? $gallery : "";
-                if ($photo!="") {
+                $photo = (isset($gallery) && $gallery != "") ? $gallery : "";
+                if ($photo != "") {
                     $ext                        = $photo->getClientOriginalExtension();
                     $gallerFullName             = time().$photo->getClientOriginalName();
                     $uploadPath                 = 'images/';
@@ -270,7 +269,7 @@ class PropertyController extends Controller
                 }
 
                 $galleriesData = new PropertyImageGallery();
-                $galleriesData->property_id =$property->id;
+                $galleriesData->property_id = $property->id;
                 $galleriesData->images = '/'.$uploadPath.$gallerFullName;
                 $galleriesData->save();
             }
@@ -283,7 +282,7 @@ class PropertyController extends Controller
 
         $property = Property::with('type', 'features.ferature', 'user', 'image_galleries', 'location')->find($id);
         return Inertia::render("Dashboard/Property/PropertyListDetails", [
-            'property' =>$property,
+            'property' => $property,
         ]);
     }
 
